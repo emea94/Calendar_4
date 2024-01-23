8e0b451fb82e0a81736c65dd40dc1400e36cfd59
R-Schreyer <roland_schreyer@web.de>
2024-01-23 18:36:32 +0100

    add one string

--- src/main/java/org/example/Calendar.java
+++ src/main/java/org/example/Calendar.java
@@ -15,7 +15,7 @@ public class Calendar {
         System.out.println("Oktober");
         System.out.println("November");
         System.out.println("Dezember");
-
+        System.out.println("ziemlich kompliziert");
 
 
     }
