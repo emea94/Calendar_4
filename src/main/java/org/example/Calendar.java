1f9f389746c04ef418a41c1018ec24d9732d5e2a
R-Schreyer <roland_schreyer@web.de>
2024-01-23 18:28:55 +0100

    Add print of months

--- src/main/java/org/example/Calendar.java
+++ src/main/java/org/example/Calendar.java
@@ -2,7 +2,21 @@ package org.example;
 
 public class Calendar {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
+        System.out.println("Januar");
+        System.out.println("Februar");
+        System.out.println("März");
+        System.out.println("April");
+        System.out.println("Mai");
+        System.out.println("Juni");
+        System.out.println("Juli");
+        System.out.println("August");
+        System.out.println("September");
+        System.out.println("Oktober");
+        System.out.println("November");
+        System.out.println("Dezember");
+
+
+
     }
 }
